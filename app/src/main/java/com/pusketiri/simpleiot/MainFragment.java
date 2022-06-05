d966a345da0e30daa8f96131fba2c51e6de4a7e3
IvanJuricic <ivanjuricic97@gmail.com>
2022-06-05 23:01:52 +0200

    working

--- app/src/main/java/com/pusketiri/simpleiot/MainFragment.java
+++ app/src/main/java/com/pusketiri/simpleiot/MainFragment.java
@@ -1,10 +1,12 @@
 package com.pusketiri.simpleiot;
 
+import android.annotation.SuppressLint;
 import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
@@ -20,10 +22,24 @@ import com.amazonaws.mobileconnectors.iot.AWSIotMqttNewMessageCallback;
 import com.amazonaws.mobileconnectors.iot.AWSIotMqttQos;
 import com.amazonaws.regions.Regions;
 
+import com.jjoe64.graphview.GridLabelRenderer;
+import com.jjoe64.graphview.helper.DateAsXAxisLabelFormatter;
+import com.jjoe64.graphview.helper.StaticLabelsFormatter;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
 import com.pusketiri.simpleiot.databinding.FragmentMainBinding;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.UnsupportedEncodingException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Objects;
+import java.util.Random;
 import java.util.UUID;
 
 /**
@@ -69,6 +85,10 @@ public class MainFragment extends Fragment {
 
     CognitoCachingCredentialsProvider credentialsProvider;
 
+    LineGraphSeries<DataPoint> temp_series, moisture_series;
+    private Thread temp_thread;
+    private boolean plot_temp = true;
+
     public MainFragment() {
         // Required empty public constructor
     }
@@ -99,6 +119,8 @@ public class MainFragment extends Fragment {
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
 
+        //requireActivity().getSupportActionBar().hide();
+
         // MQTT client IDs are required to be unique per AWS IoT account.
         // This UUID is "practically unique" but does not _guarantee_
         // uniqueness.
@@ -115,6 +137,7 @@ public class MainFragment extends Fragment {
         // MQTT Client
         mqttManager = new AWSIotMqttManager(clientId, CUSTOMER_SPECIFIC_ENDPOINT);
 
+        // Create connection
         try {
             mqttManager.connect(credentialsProvider, new AWSIotMqttClientStatusCallback() {
                 @Override
@@ -154,6 +177,9 @@ public class MainFragment extends Fragment {
             Log.e(LOG_TAG, "Connection error.", e);
             binding.mqttStatusMessage.setText("Error! " + e.getMessage());
         }
+
+        temp_series = new LineGraphSeries<DataPoint>();
+        moisture_series = new LineGraphSeries<DataPoint>();
     }
 
     @Override
@@ -162,7 +188,50 @@ public class MainFragment extends Fragment {
         // Inflate the layout for this fragment
         binding = FragmentMainBinding.inflate(inflater, container, false);
         binding.getRoot().getRootView().setBackgroundColor(Color.WHITE);
+        //binding.getRoot().getRootView().
 
+        /*binding.tempGraph.getViewport().setScalable(true); // enables horizontal zooming and scrolling
+        binding.tempGraph.getViewport().setScalableY(true); // enables vertical zooming and scrolling
+        binding.tempGraph.getViewport().setYAxisBoundsManual(true); // Prevents auto-rescaling the Y-axis
+        binding.tempGraph.getViewport().setXAxisBoundsManual(true); // Prevents auto-rescaling the X-axis
+        binding.tempGraph.setTitleTextSize(96);*/
+        /*binding.tempGraph.setTitle("Temperature measurements");
+        binding.tempGraph.setTitleColor(Color.BLACK);
+        binding.tempGraph.setTitleTextSize(64);
+        binding.tempGraph.getGridLabelRenderer().setHumanRounding(true);
+        //binding.tempGraph.getGridLabelRenderer().setVerticalAxisTitleTextSize(64);
+
+        binding.tempGraph.getGridLabelRenderer().setVerticalAxisTitle("T[°C]");
+        binding.tempGraph.getGridLabelRenderer().setVerticalLabelsVisible(true);
+        binding.tempGraph.getGridLabelRenderer().setVerticalAxisTitleColor(Color.BLACK);
+
+        binding.tempGraph.getGridLabelRenderer().setGridColor(Color.BLACK);
+        //binding.tempGraph.getGridLabelRenderer().setHorizontalAxisTitleTextSize(64);
+
+        binding.tempGraph.getGridLabelRenderer().setHorizontalAxisTitle("Time [s]");
+        binding.tempGraph.getGridLabelRenderer().setHorizontalLabelsVisible(true);
+        binding.tempGraph.getGridLabelRenderer().setHorizontalAxisTitleColor(Color.BLACK);
+
+        binding.moistureGraph.setTitle("Moisture measurements");
+        binding.moistureGraph.setTitleColor(Color.BLACK);
+        binding.moistureGraph.setTitleTextSize(64);
+        binding.moistureGraph.getGridLabelRenderer().setHumanRounding(true);
+        //binding.tempGraph.getGridLabelRenderer().setVerticalAxisTitleTextSize(64);
+
+        binding.moistureGraph.getGridLabelRenderer().setVerticalAxisTitle("Moisture");
+        binding.moistureGraph.getGridLabelRenderer().setVerticalLabelsVisible(true);
+        binding.moistureGraph.getGridLabelRenderer().setVerticalAxisTitleColor(Color.BLACK);
+
+        binding.moistureGraph.getGridLabelRenderer().setGridColor(Color.BLACK);
+        //binding.tempGraph.getGridLabelRenderer().setHorizontalAxisTitleTextSize(64);
+
+        binding.moistureGraph.getGridLabelRenderer().setHorizontalAxisTitle("Time [s]");
+        binding.moistureGraph.getGridLabelRenderer().setHorizontalLabelsVisible(true);
+        binding.moistureGraph.getGridLabelRenderer().setHorizontalAxisTitleColor(Color.BLACK);
+
+        //binding.tempGraph.addSeries(tmp);
+        //binding.moistureGraph.addSeries(tmp);
+*/
         return binding.getRoot();
         //return inflater.inflate(R.layout.fragment_main, container, false);
     }
@@ -182,13 +251,103 @@ public class MainFragment extends Fragment {
                 public void run() {
                     try {
                         String message = new String(data, "UTF-8");
-                        Log.d(LOG_TAG, "Message arrived:");
+                        if(topic.equals(topics[0])) {
+                            JSONObject obj = new JSONObject(message);
+                            int value = Integer.parseInt(obj.getString("reading"));
+                            Random rand = new Random();
+                            //value = rand.nextInt(30) % value;
+                            //int epoch = Integer.parseInt(obj.getString("timestamp"));
+                            //@SuppressLint("SimpleDateFormat")
+                            //String date = new java.text.SimpleDateFormat("MM/dd/yyyy HH:mm:ss").format(new java.util.Date(epoch*1000));
+
+                            Date currentTime = Calendar.getInstance().getTime();
+
+                            binding.tempGraph.setTitle("Temperature measurements");
+                            binding.tempGraph.setTitleColor(Color.BLACK);
+                            binding.tempGraph.setTitleTextSize(64);
+                            binding.tempGraph.getGridLabelRenderer().setHumanRounding(true);
+                            //binding.tempGraph.getGridLabelRenderer().setVerticalAxisTitleTextSize(64);
+
+                            binding.tempGraph.getGridLabelRenderer().setVerticalAxisTitle("T[°C]");
+                            binding.tempGraph.getGridLabelRenderer().setVerticalLabelsVisible(true);
+                            binding.tempGraph.getGridLabelRenderer().setVerticalAxisTitleColor(Color.BLACK);
+
+                            binding.tempGraph.getGridLabelRenderer().setGridColor(Color.BLACK);
+                            binding.tempGraph.getGridLabelRenderer().setVerticalLabelsColor(Color.BLACK);
+                            //binding.tempGraph.getGridLabelRenderer().setHorizontalAxisTitleTextSize(64);
+
+                            /*binding.tempGraph.getGridLabelRenderer().setHorizontalAxisTitle("Time [s]");
+                            binding.tempGraph.getGridLabelRenderer().setHorizontalLabelsVisible(true);
+                            binding.tempGraph.getGridLabelRenderer().setHorizontalAxisTitleColor(Color.BLACK);
+                            binding.tempGraph.getGridLabelRenderer().setHorizontalLabelsColor(Color.BLACK);
+
+*/
+                            temp_series.appendData(new DataPoint(currentTime, value), true, 500);
+                            temp_series.setColor(Color.BLUE);
+
+                            //temp_thread.
+                            binding.tempGraph.addSeries(temp_series);
+
+                            binding.tempGraph.getViewport().setMinY(10);
+                            binding.tempGraph.getViewport().setMaxY(40);
+
+                            binding.tempGraph.getViewport().setYAxisBoundsManual(true);
+                            //binding.tempGraph.getViewport().setXAxisBoundsManual(true);
+
+                            Log.d(LOG_TAG, "run: series " + value);
+
+
+                        }
+                        else if(topic.equals(topics[3])) {
+
+                            JSONObject obj = new JSONObject(message);
+                            String value = obj.getString("reading");
+                            value = value.replace("\"", "");
+                            binding.coolerStatusMessage.setText(value);
+
+                        } else if(topic.equals(topics[1])) {
+                            JSONObject obj = new JSONObject(message);
+                            int value = Integer.parseInt(obj.getString("reading"));
+                            Random rand = new Random();
+                            //value = rand.nextInt(30) % value;
+                            //int epoch = Integer.parseInt(obj.getString("timestamp"));
+                            //@SuppressLint("SimpleDateFormat")
+                            //String date = new java.text.SimpleDateFormat("MM/dd/yyyy HH:mm:ss").format(new java.util.Date(epoch*1000));
+
+                            Date currentTime = Calendar.getInstance().getTime();
+
+                            binding.moistureGraph.setTitle("Moisture measurements");
+                            binding.moistureGraph.setTitleColor(Color.BLACK);
+                            binding.moistureGraph.setTitleTextSize(64);
+                            binding.moistureGraph.getGridLabelRenderer().setHumanRounding(true);
+
+                            binding.moistureGraph.getGridLabelRenderer().setVerticalAxisTitle("Moisture");
+                            binding.moistureGraph.getGridLabelRenderer().setVerticalLabelsVisible(true);
+                            binding.moistureGraph.getGridLabelRenderer().setVerticalAxisTitleColor(Color.BLACK);
+                            binding.moistureGraph.getGridLabelRenderer().setVerticalLabelsColor(Color.BLACK);
+
+                            binding.moistureGraph.getGridLabelRenderer().setGridColor(Color.BLACK);
+
+                            moisture_series.appendData(new DataPoint(currentTime, value), true, 500);
+                            moisture_series.setColor(Color.BLUE);
+
+                            //temp_thread.
+                            binding.moistureGraph.addSeries(moisture_series);
+
+                            binding.moistureGraph.getViewport().setMinY(20);
+                            binding.moistureGraph.getViewport().setMaxY(70);
+
+                            binding.moistureGraph.getViewport().setYAxisBoundsManual(true);
+
+                            Log.d(LOG_TAG, "run: series " + value);
+                        }
+                       /* Log.d(LOG_TAG, "Message arrived:");
                         Log.d(LOG_TAG, "   Topic: " + topic);
-                        Log.d(LOG_TAG, " Message: " + message);
+                        Log.d(LOG_TAG, " Message: " + message);*/
 
-                        binding.receivedMessages.setText(message);
+                        //binding.receivedMessages.setText(message);
 
-                    } catch (UnsupportedEncodingException e) {
+                    } catch (UnsupportedEncodingException | JSONException e) {
                         Log.e(LOG_TAG, "Message encoding error.", e);
                     }
                 }
@@ -199,14 +358,6 @@ public class MainFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
-        binding.buttonNext.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                NavHostFragment.findNavController(MainFragment.this)
-                        .navigate(R.id.action_mainFragment_to_FirstFragment);
-            }
-        });
 
         binding.buttonPublish.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -226,6 +377,8 @@ public class MainFragment extends Fragment {
                     Log.d(LOG_TAG, "onClick: tu smo");
                     try {
                         mqttManager.subscribeToTopic(topics[0], AWSIotMqttQos.QOS0, new_message_callback);
+                        mqttManager.subscribeToTopic(topics[1], AWSIotMqttQos.QOS0, new_message_callback);
+                        mqttManager.subscribeToTopic(topics[3], AWSIotMqttQos.QOS0, new_message_callback);
                     } catch (Exception e) {
                         Log.e(LOG_TAG, "Subscription error.", e);
                     }
